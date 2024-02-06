9b0a85100e9b8308798f828431d49e5bd9ef32bf
Seun-A <73300414+Seun-A@users.noreply.github.com>
2024-02-06 10:11:58 +0100

    hide testimonials section

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
                 <Community />
             </div>
 
-            <Testimonials />
+            {/* <Testimonials /> */}
                 
             <section className={styles.page_section}>
                 <Merch />
